7d6d1cac449ac77767efb31d83d1c774adaa47a0
ArpanChaudhary <datascience2511@outlook.com>
2024-12-13 17:05:55 +0530

    Add files via upload

---
+++ Var1.java
@@ -0,0 +1,14 @@
+public class Var1{
+    public static void main(String[] args) {
+        int a = 10;
+        float b = (float) 0.5;
+        char c = 'a';
+        double d = 200.09;
+
+        System.out.println("Integer: " + a);
+        System.out.println("Float value: " + b);
+        System.out.println("Character is: " + c);
+        System.out.println("Double: " + d);
+
+    }
+}
