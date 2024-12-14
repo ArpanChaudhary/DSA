7be5cf3cf9cee792a61254c341cc54cff96fe4b5
ArpanChaudhary <datascience2511@outlook.com>
2024-12-14 16:04:09 +0530

    Add files via upload

---
+++ Var2.java
@@ -0,0 +1,15 @@
+
+// Declare a variable of type float named var and initialize its value to 10.45
+
+// After that re-initialize its value to 20.55 and print the variable var
+
+public class Var2{
+    public static void main(String[] args){
+        float var = 10.45f;
+
+        var = 20.55f;
+
+        System.out.println(var);
+    }
+}
+
